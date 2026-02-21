39b0c1828ac8fdde5aa0ca70aa7a883b8800fd42
Konstantin Dubikov <kokedevelopment@gmail.com>
2026-02-21 01:59:12 +0100

    Tested can_beat method. (No real test, just println)

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,15 @@ fn main() {
 
     let cards : Vec<Card> = logics::get_random_card_deck();
     let trump_suit : logics::CardSuit = logics::get_trump_suit(); 
-    for card in cards {
+    
+    for card in &cards {
         println!("{:?}", card);
     }
 
-    println!("{:?}", trump_suit);
+    println!("The trump suit is: {:?}", trump_suit);
 
+    {
+    println!("{}", cards[34].can_beat(cards[35].clone(), trump_suit));
+    }
 }
 
