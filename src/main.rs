f33224bf1e2e3d38027493cf5b85aa8078f22868
Konstantin Dubikov <kokedevelopment@gmail.com>
2026-02-22 23:46:08 +0100

    Removed .clone() in main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,6 @@ fn main() {
 
     println!("The trump suit is: {:?}", trump_suit);
 
-    println!("{}", cards[34].can_beat(&cards[35].clone(), &trump_suit));
+    println!("{}", cards[34].can_beat(&cards[35], &trump_suit));
 }
 
